3b367bdd207a425e155dc0a1b652d8bb470859cb
Cristi D <cristi.dnla@gmail.com>
2014-08-20 18:59:55 +0300

    Connect to the database when constructor is called in Db

--- src/app/db.py
+++ src/app/db.py
@@ -4,11 +4,19 @@
 #
 #
 
+import psycopg2
+
 class Db(object):
 	
 	def __init__(self, dbName, dbUser, dbPassword, dbPort, dbHost):
-		pass
-	
+		psycopg2.connect(
+			database=dbName,
+			user=dbUser,
+			password=dbPassword,
+			host=dbHost,
+			port=dbPort
+		)
+
 	def insertRecord(**kwargs):
 		pass
 
