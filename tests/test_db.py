4c5a2958841b2e901f1648fe8b84824e00c67949
Cristi D <cristi.dnla@gmail.com>
2014-08-22 17:40:17 +0300

    Connect to the database in the setUp method

--- tests/test_db.py
+++ tests/test_db.py
@@ -1,8 +1,13 @@
 import unittest
-from ..src.app.db import DB
+from src.app.db import DB
 
 class TestDB(unittest.TestCase):
 	def setUp(self):
-		pass
+		self.db = DB(dbName='test_web_store_db',
+			dbUser='cristi',
+			dbPassword='1234',
+			dbHost='localhost',
+			dbPort=5432 
+		)	
 
 
