5701fc3b8f00756dd3e2ea8231ad637776e84594
Cristi D <cristi.dnla@gmail.com>
2014-08-22 17:41:39 +0300

    Add cursor and connection attributes

--- tests/test_db.py
+++ tests/test_db.py
@@ -3,11 +3,10 @@ from src.app.db import DB
 
 class TestDB(unittest.TestCase):
 	def setUp(self):
-		self.db = DB(dbName='test_web_store_db',
+		self.dbConn = DB(dbName='test_web_store_db',
 			dbUser='cristi',
 			dbPassword='1234',
 			dbHost='localhost',
 			dbPort=5432 
 		)	
-
-
+		self.dbCur = self.dbConn()
