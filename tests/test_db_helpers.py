454d300205adcc86987d2341f9ce3f6f8d7cb406
Cristi D <cristi.dnla@gmail.com>
2014-08-22 21:00:48 +0300

    Add tests for greaterThan

--- tests/test_db_helpers.py
+++ tests/test_db_helpers.py
@@ -1,4 +1,4 @@
-from src.app.db.helpers import equals, notEquals, lessThan
+from src.app.db.helpers import equals, notEquals, lessThan, greaterThan
 import unittest
 
 class TestDBHelpers(unittest.TestCase):
@@ -13,3 +13,7 @@ class TestDBHelpers(unittest.TestCase):
 	def test_lessThan(self):
 		expectedValue = ('a < %(a)s', {'a': '1'})
 		self.assertEquals(expectedValue, lessThan('a', 1))		
+
+	def test_greaterThan(self):
+		expectedValue =	 ('a > %(a)s', {'a': '12'}) 
+		self.assertEquals(expectedValue, greaterThan('a', 12))		
