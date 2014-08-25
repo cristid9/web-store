9ca26abb81687da210dfdfd99ab4a1f38ff15655
Cristi D <cristi.dnla@gmail.com>
2014-08-25 15:04:36 +0300

    Add ProductComment class

--- src/app/product.py
+++ src/app/product.py
@@ -5,6 +5,7 @@
 #
 
 from main import db
+from datetime import datetime
 
 ##
 #
@@ -25,3 +26,31 @@ class Product(db.Model):
 
 	def __repr__(self):
 		return "<Product(%r, %r, %)>" % (self.name, self.price, self.stock)
+
+
+class ProductComments(db.Column):
+	__tablename__ = 'productcomments_table'
+
+	id = db.Column(db.Integer, primary_key=True)
+	data = db.Column(db.DateTime, nullable=False)
+	comment = db.Column(db.String, nullable=False)
+	productId = db.Column(db.Integer, db.ForeignKey('product_table.id'))
+	userId = db.Column(db.Integer, db.ForeignKey('productcomments_table.id'))
+
+	def __init__(self, user, product, comment):
+		self.user = user
+		self.product = product
+		self.comment = comment
+
+	def __repr__(self):
+		return "<ProductComment(%r, %r, %r)>" % (self.user, self.product, 
+					self.comment)
+
+
+		
+
+
+
+
+
+
