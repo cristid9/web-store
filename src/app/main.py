0a4c755bcb6d0acd5db0d5383318d6e6e617729d
Cristi D <cristi.dnla@gmail.com>
2014-08-25 14:41:53 +0300

    Add new database uri

--- src/app/main.py
+++ src/app/main.py
@@ -4,6 +4,9 @@ from flask.ext.sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.debug = True
 app.secret_key = 'kjsdhfssdkf'
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://cristi:1234@localhost/"\
+										"test_web_store_db"
+
 db = SQLAlchemy(app)
 
 
