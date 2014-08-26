8b4c80b5bd51529dff90a4055765d86d97994058
Cristi D <cristi.dnla@gmail.com>
2014-08-26 13:59:43 +0300

    Remove email field from forms.py

--- src/app/forms.py
+++ src/app/forms.py
@@ -10,7 +10,6 @@ from wtforms.validators import Required, Email, EqualTo
 class SingupForm(Form):
 	name = TextField('name', [Required()])
 	username = TextField('username', [Required()])
-	email = TextField('email', [Required()])
 	password = PasswordField('password', [
 		Required(),
 		EqualTo('confirm', message="Parolele nu se potrivesc")
