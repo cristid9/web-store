8593ecfd9a385f3cdfe91ab3391ab8735ef1e105
Cristi D <cristi.dnla@gmail.com>
2014-08-26 17:24:22 +0300

    Add config settings for email sending

--- src/app/main.py
+++ src/app/main.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.mail import Mail
 
 app = Flask(__name__)
 app.debug = True
@@ -7,6 +8,17 @@ app.secret_key = 'kjsdhfssdkf'
 app.csrf_enabled = True
 app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://cristi:1234@localhost/"\
 										"test_web_store_db"
+# Setting needed for mail server.
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
+app.config['MAIL_USERNAME'] = 'magazin.licenta@gmail.com'
+app.config['MAIL_PASSWORD'] = 'licenta123'
+app.config['ADMINS'] = ['magazin.licenta@gmail.com']
+
+# Create the mai object
+mail = Mail(app)
 
 db = SQLAlchemy(app)
 
