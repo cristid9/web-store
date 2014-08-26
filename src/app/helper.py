276f5f4fa1201a16625812056e32ab4bad8767b4
Cristi D <cristi.dnla@gmail.com>
2014-08-26 17:57:21 +0300

    Add sendMail helper function

--- src/app/helper.py
+++ src/app/helper.py
@@ -2,3 +2,12 @@
 #
 #  This file will contain all the functions and decorator that aren't related
 #  to a particular module and don't provide crucial functionality.
+
+from flask.ext.mail import Mail
+from main import mail
+
+def sendMail(subject, sender, recipients, messageBody, messageHtmlBody):
+	msg = Message(subject, sender=sender, recipients=recipients)
+	msg.body = messageBody
+	msg.html = messageHtmlBody
+	mail.send(msg)
