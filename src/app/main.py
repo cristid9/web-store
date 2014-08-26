026948cd2cd7de08ea70f0ec460d0c8fb6c0518b
Cristi D <cristi.dnla@gmail.com>
2014-08-26 13:58:01 +0300

    Add support for csrf

--- src/app/main.py
+++ src/app/main.py
@@ -4,6 +4,7 @@ from flask.ext.sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.debug = True
 app.secret_key = 'kjsdhfssdkf'
+app.csrf_enabled = True
 app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://cristi:1234@localhost/"\
 										"test_web_store_db"
 
