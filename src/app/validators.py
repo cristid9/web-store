85c996d1d0f6e18ebad9c8c1039829b7e6ad6ef2
Cristi D <cristi.dnla@gmail.com>
2014-08-26 14:50:53 +0300

    Add validator for email addres correctness

--- src/app/validators.py
+++ src/app/validators.py
@@ -6,7 +6,12 @@
 from wtforms.validators import ValidationError
 from main import db
 from user import User
+from email.utils import parseaddr
 
 def uniqueUser(form, field):
 	if User.query.filter_by(username=field.data):
 		raise ValidationError('Username is already taken')
+
+def validEmail(form, field):
+	if parseaddr(field.data)[1] is None:
+		raise ValidationError('Email address is not correct')
