82479a5c87e4411710c6c8151d48f5fd677f53bf
Cristi D <cristi.dnla@gmail.com>
2014-08-28 00:37:17 +0300

    Add helper to generate url

--- src/app/helper.py
+++ src/app/helper.py
@@ -3,7 +3,7 @@
 #  This file will contain all the functions  that aren't related
 #  to a particular module and don't provide crucial functionality.
 
-from flask.ext.mail import Mail
+from flask.ext.mail import Message
 from main import mail
 
 def sendMail(subject, sender, recipients, messageBody, messageHtmlBody):
@@ -11,3 +11,9 @@ def sendMail(subject, sender, recipients, messageBody, messageHtmlBody):
 	msg.body = messageBody
 	msg.html = messageHtmlBody
 	mail.send(msg)
+
+## We don't wan't to make dirty and log concatenations in app, to avoid this
+#  scenario I created this functions that add the remaining parts to the url,
+#  in this case the http://.
+def generateUrl(hostname, route):
+	return "http://" + hostname + route
