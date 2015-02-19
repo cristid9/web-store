631502a5d9d4b24bc263f692b079b93879a74122
Cristi D <cristi.dnla@gmail.com>
2015-02-19 15:21:36 +0200

    Implement basic logic for posting comments

---
+++ src/app/static/js/comment.js
@@ -0,0 +1,37 @@
+function Comment(poster,
+                 content,
+                 $container) {
+
+    this.poster = poster;
+    this.content = content;
+    this.$container = $container;
+    this.minLength = 10;
+    // Ajax route to post the message to the server.
+    this.route = '/publish_message';
+}
+
+Comment.prototype.isValid = function() {
+    if(this.content.length < this.minLength) {
+        // I will throw some exception on the future.
+        return false;
+    }
+    return true;
+};
+
+Comment.prototype.__sendToServer = function() {
+    $.post(this.route, {
+        poster: this.poster,
+        content: this.content
+    });
+};
+
+Comment.prototype.__addToPage = function() {
+    this.$container.prepend('<div class="row">' +
+            '<strong> ' + this.content + '</strong>' +
+        '</div>');
+};
+
+Comment.prototype.publish = function() {
+    this.__sendToServer();
+    this.__addToPage();
+};
