df82720f2494fcefc8d5ab2c7dd2bb7df7096deb
Cristi D <cristi.dnla@gmail.com>
2015-02-19 23:21:48 +0200

    Document validators.py

--- src/app/validators.py
+++ src/app/validators.py
@@ -8,16 +8,26 @@ from main import db
 from user import User
 from email.utils import parseaddr
 
-
+## Function used as validator to check if the username is
+#  unique.
+#
+#  @param form The current form.
+#  @param field The field this validator is applied to.
+#
+#  @return void It doesn't return anything.
 def uniqueUser(form, field):
 	if User.query.filter_by(username=field.data).first():
 		raise ValidationError('Username is already taken')
 
 
+## Function used a validator to check if an email address is
+#  valid.
+#
+#  @param form The current form.
+#  @param field The field this validator is applied to.
+#
+#  @return void It doesn't return anything.
 def validEmail(form, field):
 	if parseaddr(field.data)[1] is None:
 		raise ValidationError('Email address is not correct')
 
-
-def validPassword(form, field):
-	pass  # implement it later
