111d992708c39b2a00102909f31dd82666698da3
Cristi D <cristi.dnla@gmail.com>
2015-02-19 21:49:41 +0200

    Document comment.js

--- src/app/static/js/comment.js
+++ src/app/static/js/comment.js
@@ -1,4 +1,16 @@
 "use strict";
+/**
+ * @brief This class is used to represent and comment.
+ *
+ * @param poster The username of the user who posts the comment.
+ * @param content The content of the message that the user has
+ *        poster.
+ * @param $container A jQuery object representing the container
+ *        div for comments.
+ * @param productId The id of the product whose page these
+ *        comments belong to.
+ *
+ */
 function Comment(poster,
                  content,
                  $container,
@@ -14,6 +26,14 @@ function Comment(poster,
     this.productId = productId;
 }
 
+/**
+ * @brief Use this method to check if an comment object is
+ *        valid. At this moment, there is only one criterion
+ *        for validity, it shouldn't be empty.
+ *
+ * @return bool It returns `true` if the comment is valid or
+ *         `false` otherwise.
+ */
 Comment.prototype.isValid = function() {
     if(this.content.length < this.minLength) {
         // I will throw some exception on the future.
@@ -22,6 +42,13 @@ Comment.prototype.isValid = function() {
     return true;
 };
 
+/**
+ * @brief Helper method used to make the logic simpler.
+ *        This method is used to send AJAX request to the
+ *        server with the comment details.
+ *
+ * @return void It doesn't return anything.
+ */
 Comment.prototype.__sendToServer = function() {
     $.post(this.route, {
         poster: this.poster,
@@ -30,6 +57,12 @@ Comment.prototype.__sendToServer = function() {
     });
 };
 
+/**
+ * @brief Use this method to publish a comment. It will
+ *        call `__sendToServer` and will reload the page.
+ *
+ * @return void It doesn't return anything.
+ */
 Comment.prototype.publish = function() {
     this.__sendToServer();
     location.reload();
