96c925c8fd9a717f0a26415debdea5862b087edd
Cristi D <cristi.dnla@gmail.com>
2015-02-19 23:10:01 +0200

    Document helper.py

--- src/app/helper.py
+++ src/app/helper.py
@@ -7,6 +7,18 @@ from flask.ext.mail import Message
 from main import mail, app
 from src.app.decorators import async
 
+## Use this function to send an email asynchronously.
+#
+#  @param subject A string representing the subject of
+#         the mail.
+#  @param sender A string representing the email address
+#         of the sender.as
+#  @param recipients A list of strings representing the
+#         recipients of the email.
+#  @param messageBody The text body of the email.
+#  @param messageHtmlBody The html body of the mail.
+#
+#  @return void It doesn't return anything
 @async
 def sendMail(subject, sender, recipients, messageBody, messageHtmlBody):
 	with app.app_context():
